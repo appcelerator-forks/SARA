2538134f07ec4aedb016f56fb94737c1f471ffc0
jocotech <joco.tech@yahoo.com>
2016-05-16 15:58:44 -0700

    Add files via upload

---
+++ Web/conf/appc.arrowdb.development.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for development
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "development",
+      "key": "mhXFNqsT7Qq9MvC0t2mXhIz8eCTlNRxq",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user_dev",
+      "password": "0cRptpTe9qgg2Mn1Vf"
+    }
+  }
+};
---
+++ Web/conf/appc.arrowdb.production.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for production
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "production",
+      "key": "Z9NGYHWAL9XTSBgmpKxVhTaUpZqZcyvB",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user",
+      "password": "LmHe7PiZIQpAygvMO1"
+    }
+  }
+};
